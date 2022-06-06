1aa453923cb2d327756976a6e4bea8e05798967e
jaysimagegallery <81186302+jaysimagegallery@users.noreply.github.com>
2022-06-06 11:07:31 -0400

    Update updatemember.php

--- updatemember.php
+++ updatemember.php
@@ -8,7 +8,7 @@ $sql1 = "SELECT username FROM members LIMIT 1";
 	$row1 = mysqli_fetch_row($result1);
 	$adminusername =  $row1[0];
 
-$stmt1 = $dbconnect -> prepare("SELECT password FROM members WHERE username = '$adminusername'");
+$stmt1 = $dbconnect -> prepare("SELECT password FROM members WHERE username = ?");
 $stmt1 -> bind_param('s', $adminusername);
 $stmt1 -> execute();
 $stmt1 -> store_result();
@@ -41,7 +41,7 @@ if  ($musername != "" || $_SESSION['apassword'] == $adminpassword) {
 	} elseif (isset($email) && $email != "" && $counting == "0") {
 
 
-		$stmt = $dbconnect->prepare("UPDATE members SET email = '$email' WHERE username = '$username'");
+		$stmt = $dbconnect->prepare("UPDATE members SET email =  ? WHERE username = '$username'");
 		$stmt->bind_param("s", $email);
 		$stmt->execute();
 
@@ -61,7 +61,7 @@ if ($updatepassword == "" || $updatepassword2 == "" ) {
 
 		$hash = password_hash($updatepassword, PASSWORD_DEFAULT);
 
-		$stmt = $dbconnect->prepare("UPDATE members SET password = '$hash' WHERE username = '$username'");
+		$stmt = $dbconnect->prepare("UPDATE members SET password = ? WHERE username = '$username'");
 		$stmt->bind_param("s", $hash);
 		$stmt->execute();
 
@@ -102,15 +102,17 @@ if (isset($profilephoto)) {
 			 	} elseif ($mimetype == "image/jpeg" || $mimetype == "image/png" || $mimetype == "image/gif" || $mimetype == "image/bmp" || $mimetype == "image/x-ms-bmp") {
 
 
-		 			$stmt = $dbconnect->prepare("UPDATE members SET photo = '$name' WHERE username = '$username'");
-					$stmt->bind_param("s", $name);
+		 			$stmt = $dbconnect->prepare("UPDATE members SET photo = ? WHERE username = '$username'");
+		 			$null = NULL;
+					$stmt->bind_param("b", $name);
+					$stmt->send_long_data(0, file_get_contents($_FILES['photos']['tmp_name'][0])); 
 					$stmt->execute();
 
-					$stmt2 = $dbconnect->prepare("UPDATE members SET mimetype = '$mimetype' WHERE username = '$username'");
+					$stmt2 = $dbconnect->prepare("UPDATE members SET mimetype = ? WHERE username = '$username'");
 					$stmt2->bind_param("s", $mimetype);
 					$stmt2->execute();
 
-					$stmt3 = $dbconnect->prepare("UPDATE members SET photoname = '$photoname' WHERE username = '$username'");
+					$stmt3 = $dbconnect->prepare("UPDATE members SET photoname = ? WHERE username = '$username'");
 					$stmt3->bind_param("s", $photoname);
 					$stmt3->execute();
 
